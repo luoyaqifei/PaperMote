16afbfb52fbb8734c3e29da53018a60de56121e5
Mingxia Zeng <emiliezeng91@gmail.com>
2024-09-22 22:54:34 -0400

    fix: add a nicer password warning

--- app/lib/schema.ts
+++ app/lib/schema.ts
@@ -4,7 +4,7 @@ const UserSchema = z.object({
   id: z.string(),
   username: z.string().min(1, "Username is required"),
   email: z.string().email("Invalid email"),
-  password: z.string().min(8, "Password is required"),
+  password: z.string().min(8, "Password is required and must be at least 8 characters long"),
   avatar: z.string().url("Avatar is URL"),
 });
 
